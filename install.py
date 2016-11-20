999a9a838a07f19659f05e706e6a3fd8de769e1e
kevmck <klm25@njit.edu>
2016-11-20 17:21:10 -0500

    Paths for install added and tested; works as expected

--- install.py
+++ install.py
@@ -2,24 +2,24 @@ import configparser
 import os
 import sys
 
-print(sys.argv[1])
-##os.system("mkdir /home/vm164/Desktop/temp")
-##os.system("tar -xzvf /home/vm164/Desktop/testCompress.tgz -C /home/vm164/Desktop/temp")
+#print(sys.argv[1])
 
-##conf = configparser.ConfigParser()
-##conf.read("/home/vm164/Desktop/temp/testConfig.cfg")
+#ADD VERSION CHECKING!!!!
+os.system("tar -xzvf /home/vm164/temp/APIv12.tgz -C /home/vm164/temp/")
 
+conf = configparser.ConfigParser()
+conf.read("/home/vm164/temp/bundleConfig.cfg")
 
-##temp = conf.get("Test", "tempLocation")
-##audiPic = conf.get("Test", "audiPic")
-##testText = conf.get("Test", "testText")
-##
-##os.system("mv /home/vm164/Desktop/temp/audi.jpg " + audiPic)
-##os.system("mv /home/vm164/Desktop/temp/test.txt " + testText)
-##
-##os.system("rm -r /home/vm164/Desktop/testCompress.tgz")
-##os.system("rm -r " + temp)
-##
-##os.system("echo")
-##os.system("echo Operation completed successfully.")
-##os.system("exit")
+temp = conf.get("apiBundleConfig", "tempLocation")
+apiLocation = conf.get("apiBundleConfig", "apiCode")
+logLocation = conf.get("apiBundleConfig", "logCode")
+
+os.system("mv " + temp + "/apiServer.php " + " " + apiLocation)
+os.system("mv " + temp + "/logRabbitMQ.ini " + " " + logLocation)
+
+os.system("rm -r /home/vm164/temp/APIv12.tgz")
+os.system("rm -r /home/vm164/temp/bundleConfig.cfg")
+
+os.system("echo")
+os.system("echo Operation completed successfully.")
+os.system("exit")
