466c077003b08973df476c47287624460c3989d5
kevmck <klm25@njit.edu>
2016-11-21 22:52:55 -0500

    Added some versioning code; needs testing

--- install.py
+++ install.py
@@ -5,7 +5,10 @@ import sys
 #print(sys.argv[1])
 
 #ADD VERSION CHECKING!!!!
-os.system("tar -xzvf /home/vm164/temp/APIv12.tgz -C /home/vm164/temp/")
+ver = (os.popen("php /home/vm164/bundleListener/rabbitMQClient.php bundleRequest").read())
+ver = ver.strip()
+
+os.system("tar -xzvf /home/vm164/temp/APIv" + ver + ".tgz -C /home/vm164/temp/")
 
 conf = configparser.ConfigParser()
 conf.read("/home/vm164/temp/bundleConfig.cfg")
@@ -17,7 +20,7 @@ logLocation = conf.get("apiBundleConfig", "logCode")
 os.system("mv " + temp + "/apiServer.php " + " " + apiLocation)
 os.system("mv " + temp + "/logRabbitMQ.ini " + " " + logLocation)
 
-os.system("rm -r /home/vm164/temp/APIv12.tgz")
+os.system("rm -r /home/vm164/temp/APIv" + ver + ".tgz")
 os.system("rm -r /home/vm164/temp/bundleConfig.cfg")
 
 os.system("echo")
